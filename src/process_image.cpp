57d146c3cf033ee30050785998fca9a07c6dc039
Luc zeng <luc.zeng@hotmail.fr>
2019-08-14 02:10:45 +0200

    Started Hough transform function

--- src/process_image.cpp
+++ src/process_image.cpp
@@ -16,8 +16,20 @@ void normalise_img(MatrixXf & img){
 	low.setZero(img.rows(),img.cols());
 
 	MatrixXf tmp = (img.array() > 128.0).select(high,low);
-	std::cout<<tmp<<std::endl;
 
 	img = tmp;
+}
+
+void hough_transform(MatrixXi & img,VectorXf theta,VectorXf rho){
+	MatrixXi acc; //accumulator
+
+	//Compute Hough transform
+	for (int i=0;i<img.rows();++i){
+		for (int j=0;j<img.cols();++j){
+			if (img(i,j) !=0){
+			//generate sinusoidal curve	
+			}
+		}
+	}
 }
 
