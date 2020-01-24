930bc2f1ae6b1393892148b67dc3309e7d7d5348
Luc zeng <luc.zeng@hotmail.fr>
2020-01-25 00:34:00 +0100

    Created util file for eigen

---
+++ include/eigen_utils.hpp
@@ -0,0 +1,32 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <vector>
+#include <array>
+
+/**
+ * Function to make sure binary is 0 and 255
+ *
+ * @param img Eigen float matrix to be normalized
+ */
+void normalise_img(Eigen::MatrixXf &img);
+
+/*
+ * Returns a linearly spaced array
+ *
+ * @param a starting value
+ * @param b end value
+ * @param N number of bins
+ * @return vector<float>
+ */
+// TODO(luczeng): this is a perfect use case for a template
+std::vector<float> LinearSpacedArray(const float &a, const float &b, const std::size_t &N);
+
+/*
+ * Finds position of all elements superior to threshold
+ *
+ * @param hough Eigen float matrix
+ * @float threshold float
+ * @return vector of Eigen::Index of the positions where hough is more than threshold
+ */
+std::vector<std::array<int, 2>> find_local_maximum(
+    const Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> &img, const float &threshold);
